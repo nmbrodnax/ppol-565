9158a37e48948dd67887a80c3c7c21beee4984a0
annnvv <13822260+annnvv@users.noreply.github.com>
2019-02-21 00:30:50 -0500

    Update merging.R

--- misc_resources/merging/merging.R
+++ misc_resources/merging/merging.R
@@ -1,5 +1,5 @@
   # Merging (using base R)
-  # The purpose of this script is to demonstrate examples of the four different types of joins (inner, outer, left, and right) to aid in the comprehension of merging.
+  # The purpose of this script is to demonstrate examples of the four different types of joins (inner, outer, left, and right) to aid in the comprehension of joining (i.e. merging).
   
   # Merge documentation
   ?merge
@@ -11,7 +11,7 @@
     #View(df2)
   # Note: Between both dataframes, there are seven unique people. Three are both in df1 and df2. Two are only in df1 and two are only in df2.
   
-  # Inner join (only joins the data from variables in both datasets for the Names that are in common between df1 and df2)
+  # Inner join (joins the data from variables in both datasets only for the Names that are in common between df1 and df2.)
   inner <- merge(df1, df2, all = FALSE, by = "Name")
     #View(inner)
   
@@ -22,14 +22,14 @@
   # Left join (joins data from df2 to df1, but only for the Names that are in df1, again take note of the missing data and the order in which the variables are the dataframe)
   # Note: left and left2 are identical objects in terms of the data that they contain. Take note of the order in which the dataframes are specified and whether all.x or all.y is used.
   left <- merge(df1, df2, all.x = TRUE, by = "Name")
-  left2 <- merge(df2, df1, all.y = TRUE, by = "Name")
+  right <- merge(df2, df1, all.y = TRUE, by = "Name")
     #View(left)
     #View(left2)  
   
   # Right join (join data from df1 to df2, but only for the Names that are in df2, again take note of the missing data and the order in which the variables are the dataframe)
   # Note: right and right2 are identical objects in terms of the data that they contain. Take note of the order in which the dataframes are specified and whether all.x or all.y is used.
-  right <- merge(df1, df2, all.y = TRUE, by = "Name")
-  right2 <- merge(df2, df1, all.x = TRUE, by = "Name")
+  right2 <- merge(df1, df2, all.y = TRUE, by = "Name")
+  left2 <- merge(df2, df1, all.x = TRUE, by = "Name")
     #View(right)
     #View(right2)
   
