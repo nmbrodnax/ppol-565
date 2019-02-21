3038990f6d2f99dc3926c567015517572ae5dffb
annnvv <13822260+annnvv@users.noreply.github.com>
2019-02-21 00:35:46 -0500

    Update merging.R

--- misc_resources/merging/merging.R
+++ misc_resources/merging/merging.R
@@ -24,13 +24,13 @@
   left <- merge(df1, df2, all.x = TRUE, by = "Name")
   right <- merge(df2, df1, all.y = TRUE, by = "Name")
     #View(left)
-    #View(left2)  
+    #View(right)  
   
   # Right join (join data from df1 to df2, but only for the Names that are in df2, again take note of the missing data and the order in which the variables are the dataframe)
   # Note: right and right2 are identical objects in terms of the data that they contain. Take note of the order in which the dataframes are specified and whether all.x or all.y is used.
   right2 <- merge(df1, df2, all.y = TRUE, by = "Name")
   left2 <- merge(df2, df1, all.x = TRUE, by = "Name")
-    #View(right)
     #View(right2)
+    #View(left2)
   
   # Note: if the variable you are joining on is named differently in the two datasets, you will need to specify both by.x = "name1" and by.y = "name2".
